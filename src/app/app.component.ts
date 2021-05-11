670379f952b27d5da58f084ffb550e3888e8302c
Li <steven.li@efrei.net>
2021-05-11 15:17:24 +0200

    Backend connection + Unit Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiService} from './service/api.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProductHuntFrontend';
+
+  constructor(private apiService : ApiService){
+    apiService.getProductsByDate("2021-03-03").then((res)=>{
+      console.log(res)
+    })
+  }
+
+
 }
